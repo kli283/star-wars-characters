c77dc9755a70de734cac711a7b9bc0f2a8ef04fb
kli283 <kenny.li.1997@hotmail.com>
2022-09-06 21:07:36 +1000

    Initial commit with react files

---
+++ star-wars-characters/src/views/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+import {Container} from "@mui/material";
+
+export const MainPage = () => {
+    return (
+        <Container>
+            <h1>Star Wars characters</h1>
+        </Container>
+    );
+};
